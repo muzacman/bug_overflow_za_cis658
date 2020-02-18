7dbabf733db5641cbce24e1abef9400561f13a48
[ZachArens] <[arens.zach@gmail.com]>
2020-02-17 20:47:20 -0500

    Bootstap and formatting finished, testing and debugging

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ class User < ApplicationRecord
   #TODO - need to update validation to pass tests
   # VALID_EMAIL_REGEX = URI::MailTo::EMAIL_REGEXP
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_THUMBNAIL_REGEX = /[\\\:\/a-zA-Z]+\.(gif|jpg|png)\Z/
   validates :email, presence: true, length: {maximum: 105},
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
 
   validates :thumbnail, allow_blank: true,
-            format: {with: /[\\\:\/a-zA-Z]+\.(gif|jpg|png)\Z/,
+            format: {with: VALID_THUMBNAIL_REGEX,
                      message: 'must be a URL for GIF, JPG, or PNG image.'}
 
   has_many :bugs
